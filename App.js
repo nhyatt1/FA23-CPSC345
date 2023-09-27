33ef58eec577f315dd92a134e41bc4b1ef8001fd
nhyatt1 <nhyatt1@muskingum.edu>
2023-09-27 14:44:12 -0400

    9/27 Ex2

--- App.js
+++ App.js
@@ -1,25 +1,35 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {CipherScreen} from './cipher/cipher';
+import { CipherScreen } from './cipher/cipher';
 import { styles } from './styles';
+import { HistoryScreen } from './history';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+import { ActivityIndicator } from 'react-native';
 
-const Stack = createNativeStackNavigator();
+const persistor = persistStore(store)
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-        name="Home"
-        component={CipherScreen}
-        options={{title: 'Home Page'}}
-        />
-        <Stack.Screen
-        name="History"
-        component={}
-        options={{title: 'History Page'}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store = {store}>
+      <PersistGate loading={<ActivityIndicator/>} persistor={persistor}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={CipherScreen}
+              options={{title: 'Home Page'}}
+            />
+            <Stack.Screen
+              name="History"
+              component={HistoryScreen}
+              options={{title: 'History Page'}}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   )
 }
--- history.js
+++ history.js
@@ -1,5 +1,11 @@
 import { styles } from './styles';
 
 export function HistoryScreen({route, navigation}){
-
+    return (
+        <View style={styles.container}>
+            <Button title='To Cipher'
+                onPress ={() => }
+            />
+        </View>
+      );
 }
